d24f74e4eb1090966ce8df27ec4e0069f74c6238
Radu Manea <30615642+radubm1@users.noreply.github.com>
2023-08-27 18:22:23 +0300

    Add lambda extension

---
+++ sem.js
@@ -0,0 +1,40 @@
+'use strict';
+/*
+ * Abstractions:
+ */
+const walk = x => true; //vi
+const beat = (x,y) => true; //vt
+const exist = x => x; //there is - operator
+const any = x => x; //any - op
+const is = (x,y) => true;
+const not = x => !x; //- op ?
+/*
+ * Operators:
+ */
+let Or = "||";
+let And = '&&';
+/*
+ * Applications: binding
+ */
+let John = "is('man','john')"; //&&
+let Walk = "walk(any('man'))"; //Function()
+let Not = "not(true)"; //&&
+let Beat = "beat('john','drums')";
+let Any = "any('john')";
+/*
+ * Abstraction & Applications
+ */
+ /* let John = ((x,y) => true)('john','man');
+ * let Walk = (x => (x => true))('man');
+ * let Not = (x => !x)(true);
+ * console.log(John && Walk && Not);
+ */
+/*
+ * Evaluations:
+ */
+//console.log(beat(any('man'),'drums'));
+//console.log(not(is(any('woman'),'beautiful')));
+//any man beat drums
+//console.log(eval(John + Walk + Or + Not + Beat));
+let s = process.argv[2];
+console.log(eval(eval(s.split(' ').map(x=>{return x;}).join('+ And +')))); //+?
